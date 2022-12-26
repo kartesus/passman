2a8754cb480859c60b977bc497366bf383f150ca
Alex Gravem <gravem@gmail.com>
2022-12-26 14:55:38 -0300

    done

--- src/passman/app.clj
+++ src/passman/app.clj
@@ -1,15 +1,26 @@
 (ns passman.app
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [passman.db :refer [list-passwords]]))
+            [passman.db :refer [list-passwords insert-password]]
+            [passman.password :refer [generate-password]]
+            [table.core :as t]))
 
 (def cli-options
-  [[nil "--list"]])
+  [["-l" "--length LENGTH" "Length of the password" :default 20 :parse-fn #(Integer/parseInt %)]
+   ["-g" "--generate" "Generate a password"]
+   [nil "--list" "List all passwords"]
+   ["-h" "--help" "Show help"]])
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (let [opts (:options (parse-opts args cli-options))]
+  (let [parsed-opts (parse-opts args cli-options)
+        opts        (:options parsed-opts)
+        url         (first (:arguments parsed-opts))
+        username    (second (:arguments parsed-opts))]
     (cond
-      (:list opts) (list-passwords)
-      :else (println "help"))))
+      (:list opts)     (t/table (list-passwords))
+      (:generate opts) (let [password (generate-password (:length opts))]
+                         (when (and url username)
+                           (insert-password url username password)
+                           (println "Saving password to database...")))
+      :else            parsed-opts)))
 
